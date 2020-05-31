1fff9250271de4c7070cfb191290af76ceeb2dd3
Alex Leonhart <alexleonhartcode@gmail.com>
2020-05-30 17:44:09 -0700

    adding connection to sqlite

---
+++ db/schemas.py
@@ -0,0 +1,21 @@
+# Pydantic models
+
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True

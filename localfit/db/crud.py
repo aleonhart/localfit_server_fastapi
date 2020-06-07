809d1e0feda1a57197abf72b1fc6c34029363838
Alex Leonhart <alexleonhartcode@gmail.com>
2020-06-07 14:45:25 -0700

    adding ability to view all activities

--- localfit/db/crud.py
+++ localfit/db/crud.py
@@ -1,18 +1,17 @@
 from sqlalchemy.orm import Session
 
-from . import models
+from localfit.db.models import ActivityFile
 from localfit import schemas
 
 
-def create_activity(session: Session, activity_file: schemas.ActivityFile, activity_session: schemas.ActivitySession):
-    activity_file_obj = models.ActivityFile(**activity_file.dict())
+def create_activity(session: Session, activity_file: schemas.ActivityFile):
+    activity_file_obj = ActivityFile(**activity_file.dict())
     session.add(activity_file_obj)
     session.commit()
     session.refresh(activity_file_obj)
 
-    activity_session_obj = models.ActivitySession(**activity_session.dict(), file_id=activity_file_obj.id)
-    session.add(activity_session_obj)
-    session.commit()
-    session.refresh(activity_session_obj)
-
     return activity_file_obj
+
+
+def get_activities(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(ActivityFile).offset(skip).limit(limit).all()
--- localfit/routes.py
+++ localfit/routes.py
@@ -2,6 +2,7 @@
 from fastapi import Depends, File, UploadFile
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
+from typing import List
 
 # local
 from localfit.db import crud
@@ -13,7 +14,13 @@ from localfit.activities.fileupload import get_activity_data
 router = APIRouter()
 
 
-@router.post("/uploadfile/", response_model=schemas.ActivityFile)
+@router.post("/activities/", response_model=schemas.ActivityFile)
 async def create_upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    activity_file, activity_session = get_activity_data(file)
-    return crud.create_activity(session=db, activity_file=activity_file, activity_session=activity_session)
+    activity_file = get_activity_data(file)
+    return crud.create_activity(session=db, activity_file=activity_file)
+
+
+@router.get("/activities/", response_model=List[schemas.ActivityFile])
+def get_activities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_activities(db, skip=skip, limit=limit)
+    return items

01bb0bc7e02275a6238457b828bd217e76de9eea
Alex Leonhart <alexleonhartcode@gmail.com>
2020-07-25 14:52:34 -0700

    fixing display for distance and duration

--- localfit/activities/formatters/retrieval.py
+++ localfit/activities/formatters/retrieval.py
@@ -20,8 +20,8 @@ def get_activity_metadata_by_filename(db, filename):
         'activity_type': activity.activity_type,
         'start_time_utc': format_datetime_for_display(localize_datetime_for_display(activity.start_time_utc)),
         'activity_collection': activity.activity_collection,
-        'total_elapsed_time': activity.total_elapsed_time,
-        'total_distance': activity.total_distance,
+        'total_timer_time': format_duration_for_display(activity.total_timer_time),
+        'total_distance': format_distance_for_display(activity.total_distance),
         'total_calories': activity.total_calories,
         'start_location': activity.start_location,
     }
